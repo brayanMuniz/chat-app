29effd7dcb024b792d72d22d7d578e2165a03510
Brayan Muniz <35124364+brayanMuniz@user.noreply.github.com>
2019-01-24 14:50:52 -0600

    length limit

--- src/views/roomFolder/room.js
+++ src/views/roomFolder/room.js
@@ -43,7 +43,7 @@ export default {
                 roomId: this.roomUID,
             }
 
-            if (this.newMessage == null) {
+            if (this.newMessage == null || this.newMessage.length > 50) {
                 alert('stop it')
             } else {
                 this.newMessage = null
