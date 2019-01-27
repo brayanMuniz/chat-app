d60cc7540afa42187adcb594cef6d933e1ae67a3
Brayan Muniz <35124364+brayanMuniz@user.noreply.github.com>
2019-01-27 10:27:01 -0600

    added storage module

--- src/store.js
+++ src/store.js
@@ -3,11 +3,12 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 import chatModule from "./storeModules/chatModule";
 import userModule from './storeModules/userModule'
-
+import storageModule from './storeModules/storageModule'
 export default new Vuex.Store({
   modules: {
     chatModule: chatModule,
-    userModule: userModule
+    userModule: userModule,
+    storageModule: storageModule
   },
   state: {},
   getters: {},
---
+++ src/storeModules/storageModule.js
@@ -0,0 +1,20 @@
+import firebase from '../firebaseConfig'
+let storage = firebase.storage
+const state = {
+
+}
+const getters = {
+
+}
+const mutations = {
+
+}
+const actions = {
+
+}
+export default {
+    actions,
+    mutations,
+    getters,
+    state
+}
