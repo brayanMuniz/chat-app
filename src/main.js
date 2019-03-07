ff03ec03f531d67e6f72849447e6343efd8fee4c
Brayan Muniz <35124364+brayanMuniz@user.noreply.github.com>
2019-03-07 05:53:04 -0600

    removed unused icons

--- src/main.js
+++ src/main.js
@@ -7,28 +7,6 @@ import VeeValidate from 'vee-validate';
 import Vuetify from "vuetify";
 import 'vuetify/dist/vuetify.min.css'
 Vue.use(Vuetify)
-
-import {
-  library
-} from '@fortawesome/fontawesome-svg-core'
-import {
-  faCoffee,
-  faCheck,
-  faPlus,
-  faTimesCircle
-} from '@fortawesome/free-solid-svg-icons'
-import {
-  faJs,
-  faVuejs
-} from '@fortawesome/free-brands-svg-icons';
-
-import {
-  FontAwesomeIcon
-} from '@fortawesome/vue-fontawesome'
-
-library.add(faCoffee, faJs, faVuejs, faCheck, faPlus, faTimesCircle)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(VeeValidate);
 
 Vue.config.productionTip = true;
